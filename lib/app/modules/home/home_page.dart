35f5b36cb36abe007e1c948cfb195e055cb49097
Crístofer Domingues <cristofer.domingues@gmail.com>
2020-06-01 21:54:43 -0300

    Home Screen

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:cristofer_flutter_template/app/modules/home/home_controller.dart';
+import 'package:cristofer_flutter_template/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 
 final homeController = HomeController();
@@ -15,6 +17,51 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+        body: Padding(
+      padding: EdgeInsets.only(left: 20, top: 50, right: 20),
+      child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                SvgPicture.asset("assets/icons/menu.svg"),
+                Image.asset("assets/images/user.png"),
+              ],
+            ),
+            SizedBox(height: 30),
+            Text("Hey Alex,", style: kHeadingextStyle),
+            Text("Find a course you want to learn", style: kSubheadingextStyle),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 30),
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+              height: 60,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Color(0xFFF5F5F7),
+                borderRadius: BorderRadius.circular(40),
+              ),
+              child: Row(
+                children: <Widget>[
+                  SvgPicture.asset("assets/icons/search.svg"),
+                  SizedBox(width: 16),
+                  Text(
+                    "Search for anything",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Color(0xFFA0A5BD),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ]),
+    ));
+  }
+}
+
+/* 
     return Scaffold(
         appBar: AppBar(
           title: Text(widget.title),
@@ -32,7 +79,7 @@ class _HomePageState extends State<HomePage> {
               Observer(
                 builder: (BuildContext context) {
                   return Text('${homeController.value}',
-                      style: Theme.of(context).textTheme.display1);
+                      style: Theme.of(context).textTheme.headline1);
                 },
               ),
             ],
@@ -43,5 +90,4 @@ class _HomePageState extends State<HomePage> {
           tooltip: 'Increment',
           child: Icon(Icons.add),
         ));
-  }
-}
+ */
